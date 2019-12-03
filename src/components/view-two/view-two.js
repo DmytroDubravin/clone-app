8034d860889d2fc521f02efc41e95f0dc0888087
Dmytro Dubravin <Dmytro_Dubravin@epam.com>
2019-12-03 14:50:27 +0200

    try 1

---
+++ src/components/view-two/view-two.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from '@commercetools-frontend/ui-kit';
+import messages from './messages';
+
+const ViewTwo = () => <Text.Body intlMessage={messages.title} />;
+ViewTwo.displayName = 'ViewTwo';
+
+export default ViewTwo;
